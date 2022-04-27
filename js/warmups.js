b2fe0c71b039742217e09023c0dd22d873ac2b39
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-04-27 15:17:32 -0500

    forms

--- js/warmups.js
+++ js/warmups.js
@@ -1,3 +1,20 @@
 "use strict";
 
 console.log("Yay! I completed the warm-up!")
+
+let allFirstLetter = ["cat", "dog", "pig", "cow"];
+allFirstLetter.forEach(function(element, index,array) {
+    console.log()
+});
+
+
+*/Instructor solution *
+function allFirstLetters(arr) {
+    let bucket = "";
+    for (let el of arr) {
+        bucket += str.substring(0,1);
+    }
+    return bucket
+}
+const testArrStrings = ["cats","dogs","bats","rats"];
+console.log(allFirstLetters(testArrStrings));
