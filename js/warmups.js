f232428e1929e2272efdf6c422d3fcd098977fac
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-04-28 09:05:17 -0500

    warmup

--- js/warmups.js
+++ js/warmups.js
@@ -1,20 +1,42 @@
 "use strict";
-
-console.log("Yay! I completed the warm-up!")
-
-let allFirstLetter = ["cat", "dog", "pig", "cow"];
-allFirstLetter.forEach(function(element, index,array) {
-    console.log()
-});
-
-
-*/Instructor solution *
-function allFirstLetters(arr) {
-    let bucket = "";
-    for (let el of arr) {
-        bucket += str.substring(0,1);
+// console.log("Yay! I completed the warmup!");
+// // fizz divisible by 3, buzz divisible by 5, fizzbuzz by both, log num if divisible by neither
+function fizzBuzz(){
+for (let i = 1; i < 100; i ++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else if (i % 3 != 0 && i % 5 != 0) {
+        console.log(i);
+         }
     }
-    return bucket
 }
-const testArrStrings = ["cats","dogs","bats","rats"];
-console.log(allFirstLetters(testArrStrings));
+fizzBuzz();
+
+//instructor method
+//
+//  function fizzBuzz(limit){
+// if (limit === undefined) {
+//     limit = 100;
+// }
+//     for (let i = 1; i <= 100; i ++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz")
+//         } else if (i % 3 === 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+// fizzBuzz();
+
+function alphaBet(str) {
+    return str.split('').sort().join('');
+}
+console.log(alphaBet("watermelon"));
