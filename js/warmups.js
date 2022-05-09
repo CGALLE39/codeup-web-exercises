3f9ae06a7e99e54aca44eda53f5a17c973a2711b
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-05-09 09:30:58 -0500

    warmup

--- js/warmups.js
+++ js/warmups.js
@@ -86,120 +86,167 @@
 //     }
 // }
 
-(
-    function() {
-        console.log("hello kitty")
-        let str = "string";
-        let num = 'number';
-        let boolean = true;
+// (
+    // function() {
+    //     console.log("hello kitty")
+    //     let str = "string";
+    //     let num = 'number';
+    //     let boolean = true;
+    //
+    //     function identifyVal(input) {
+    //         return typeof input;
+    //     }
+    //     console.log(identifyVal("meow"));
+    //     console.log(identifyVal(1));
+    //     console.log(identifyVal('boolen'))
+    //
+    // function square(input) {
+    //         return input*input;
+    // }
+    // console.log(square(5));
+    //
+    //     function addString(input) {
+    //         return str + prompt("type in something funny");
+    //     }
+    //     console.log(addString(prompt));
+    //
+    //
+    //     function trueOrFalse(input) {
+    //         if(input === true) {
+    //             return "Cynthia likes apples"
+    //         } else if (input === false) {
+    //             return "Cynthia hates apples"
+    //         }
+    //     }
+    //
+    //     console.log(trueOrFalse(true));
+    //     console.log(trueOrFalse(false));
+    //
+    //     function lengthofString(input) {
+    //         return input.length;
+    //     }
+    //     console.log(lengthofString('coconut'));
+    //
+    //     const numbersArray = [1,2,3,4,34];
+    //     function arrayLength(input) {
+    //         return input.length;
+    //     }
+    //     console.log(arrayLength(numbersArray));
+    //     console.log(arrayLength('wewe', 4, 2, 4));
+    //
+    //     function isNumber(input){
+    //         if(!isNaN(parseFloat(input))){
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }
+    //
+    //     }
+    //
+    //     function addTwoNumbers(x,y){
+    //         if(isNumber(x) && isNumber(y)) {
+    //             return parseFloat(x) + parseFloat(y);
+    //         } else
+    //             return false;
+    //     }
+    //     console.log(addTwoNumbers(3,9));
+    //
+    //     const array1 = [3,5,2,6,8];
+    //     const array2 = ['frank', 231, false];
+    //     const array3 = ['123fdsfsdf', '3434', 'erer'];
+    //     const array4 = ['sdfsdf', 'erssdfdsff'];
+    //
+    //     function arryNum(input) {
+    //         for(let i = 0; i < input.length; i++) {
+    //             if(isNumber(input[i])  === false) {
+    //                 return false;
+    //             }
+    //         }
+    //         return true;
+    //     }
+    //
+    //     console.log(arryNum(array1));
+    //     console.log(arryNum(array2));
+    //     console.log(arryNum(array3));
+    //     console.log(arryNum(array4));
+    //
+    //     function addToEndArray(input){
+    //         array1.push(input);
+    //     }
+    //    addToEndArray('23');
+    //
+    //
+    //     function deleteLastArray(){
+    //         array1.pop();
+    //     }
+    //
+    //
+    //     //objects
+    //
+    //     const superNumber = {value:5, negPos:'positive', integer:true};
+    //     const superNumber2 = {value:-43, negPos:'negative', integer:false};
+    //
+    //     console.log(superNumber2);
+    //
+    //     function makeFavorite(obj) {
+    //         obj.favorite = true;
+    //         superNumber.favorite = true;
+    //     }
+    //     console.log(superNumber);
+    //     console.log(superNumber2);
+    //
+    //     //ppulling info from object//
+    //     function selectInfo(obj) {
+    //         return {value:obj, negPos:obj.negPos};
+    //     }
+    //     console.log(selectInfo(superNumber));
 
-        function identifyVal(input) {
-            return typeof input;
-        }
-        console.log(identifyVal("meow"));
-        console.log(identifyVal(1));
-        console.log(identifyVal('boolen'))
-
-    function square(input) {
-            return input*input;
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "Spetember 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
     }
-    console.log(square(5));
+];
 
-        function addString(input) {
-            return str + prompt("type in something funny");
+function isTallest(arr) {
+    let tallest = {heightinMM: 0};
+    for (let hamster of arr) {
+        if (hamster.heightInMM > tallest.heightInMM) {
+            tallest = hamster;
         }
-        console.log(addString(prompt));
+    }
+    return tallest;
+}
+    console.log(isTallest(hamsters));
 
 
-        function trueOrFalse(input) {
-            if(input === true) {
-                return "Cynthia likes apples"
-            } else if (input === false) {
-                return "Cynthia hates apples"
-            }
-        }
-
-        console.log(trueOrFalse(true));
-        console.log(trueOrFalse(false));
-
-        function lengthofString(input) {
-            return input.length;
-        }
-        console.log(lengthofString('coconut'));
-
-        const numbersArray = [1,2,3,4,34];
-        function arrayLength(input) {
-            return input.length;
-        }
-        console.log(arrayLength(numbersArray));
-        console.log(arrayLength('wewe', 4, 2, 4));
-
-        function isNumber(input){
-            if(!isNaN(parseFloat(input))){
-                return true;
-            } else {
-                return false;
-            }
-
-        }
-
-        function addTwoNumbers(x,y){
-            if(isNumber(x) && isNumber(y)) {
-                return parseFloat(x) + parseFloat(y);
-            } else
-                return false;
-        }
-        console.log(addTwoNumbers(3,9));
-
-        const array1 = [3,5,2,6,8];
-        const array2 = ['frank', 231, false];
-        const array3 = ['123fdsfsdf', '3434', 'erer'];
-        const array4 = ['sdfsdf', 'erssdfdsff'];
-
-        function arryNum(input) {
-            for(let i = 0; i < input.length; i++) {
-                if(isNumber(input[i])  === false) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        console.log(arryNum(array1));
-        console.log(arryNum(array2));
-        console.log(arryNum(array3));
-        console.log(arryNum(array4));
-
-        function addToEndArray(input){
-            array1.push(input);
-        }
-       addToEndArray('23');
 
 
-        function deleteLastArray(){
-            array1.pop();
-        }
 
-
-        //objects
-
-        const superNumber = {value:5, negPos:'positive', integer:true};
-        const superNumber2 = {value:-43, negPos:'negative', integer:false};
-
-        console.log(superNumber2);
-
-        function makeFavorite(obj) {
-            obj.favorite = true;
-            superNumber.favorite = true;
-        }
-        console.log(superNumber);
-        console.log(superNumber2);
-
-        //ppulling info from object//
-        function selectInfo(obj) {
-            return {value:obj, negPos:obj.negPos};
-        }
-        console.log(selectInfo(superNumber));
-
-
-}) ();
