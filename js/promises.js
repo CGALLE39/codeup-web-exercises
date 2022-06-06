b5b5229041d5ae27ed7853d027efb1db3fb2603c
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-06-06 11:03:20 -0500

    promisecomplete

--- js/promises.js
+++ js/promises.js
@@ -6,8 +6,8 @@
 //returning just the date of the last commit that user made
 
 
-const gitUser = fetch('https://api.github.com/users');
-
+const gitUser = fetch('https://api.github.com/users/CGALLE39/events/public')
+.then((resp) => resp.json())
 
 // fetch('https://api.github.com/users/events/public',{headers: {'Authorization': 'PROMISE_KEY'}}).then(gitHub => gitHub.json());
 
@@ -17,9 +17,19 @@ const gitUser = fetch('https://api.github.com/users');
 
 console.log(gitUser);
 
-//23
+let userNAme = prompt("Enter a username");
 
-function getCommit() {
-    const promise = fetch('https://api.github.com/users', {headers: {'Authorization': 'PROMISE_KEY'}}).then(gitHub => gitHub.json());
-
+function getCommit(str) {
+    const promise = fetch('https://api.github.com/users/' + str + '/events/public', {headers: {'Authorization': 'PROMISE_KEY'}})
+        .then(gitHub => gitHub.json())
+        .then((data) => {
+            console.log(data)
+            let lastCommit = {
+                commit: `${data[0].payload.commits[0].url}`
+            }
+            return lastCommit;
+        })
+        .then(user => console.log(user))
+        .catch(error => console.log(error));
 }
+    console.log(getCommit(userNAme));
