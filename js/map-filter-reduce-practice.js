0d896714cf110de48cc30d768a8561c40e4bcc17
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-06-16 18:59:15 -0500

    missing work

--- js/map-filter-reduce-practice.js
+++ js/map-filter-reduce-practice.js
@@ -50,3 +50,27 @@ const letterAlign = letters.flat().reduce((accumulator, currentValue) => {
 }, {});
 
 console.log(letterAlign);
+
+if (true) {
+    let x = 5;
+}
+console.log(typeof x);
+//
+// let numbers = [1, 2, 3, 4, 5];
+// const numbersMap = numbers.map(n => n * 3)
+// console.log(numbersMap);
+
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const isDivisibleBy3 = n => n % 3 === 0;
+console.log(numbers.filter(isDivisibleBy3));
+console.log(numbers);
+
+function sayHello(name) {
+    return 'Hello, ' + name + '!';
+}
+console.log(sayHello("Cynthia"
+));
+const sayMello = (name) => `Hello, ${name}!`
+console.log(sayMello("Cynthia"));
